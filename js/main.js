3028c70fe59666dd7ea0c587995f0b708562b29f
arielleon305 <arielmartins305@gmail.com>
2022-03-29 20:58:56 -0300

    Inicio
    Puesta en marcha de los archivos y configuraciones HTML

---
+++ js/main.js
@@ -0,0 +1,5 @@
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
