4cfa3e45052f139a18267705cb06bd44c9a540b2
arielleon305 <arielmartins305@gmail.com>
2022-04-02 10:06:47 -0300

    Actualizacion diseño

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
-La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat"
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+var botonEncriptar = document.querySelector("#encriptar");
+
+botonEncriptar.addEventListener("click",function(event){
+    event.preventDefault();
+    var textoEncriptado = encriptarTexto(leerTextoArea());
+    escribirTexto(textoEncriptado);    
+});
+
+var botonDesencriptar = document.querySelector("#desencriptar");
+botonDesencriptar.addEventListener("click",function(event){
+    event.preventDefault();  
+    alert("se presiono el boton desencriptar");
+});
+
+var botonCopiar = document.querySelector("#copiar");
+botonCopiar.addEventListener("click",function(event){
+    event.preventDefault();  
+    alert("se presiono el boton copiar");
+});
+
+//copiar texto del textarea:
+
+function leerTextoArea(){
+    var copiarTexto = document.querySelector("#mensaje");
+    return (copiarTexto.value);
+}
+
+function encriptarTexto(){
+    var texto = leerTextoArea();
+
+        
+}
+
+function escribirTexto(){
+
+}
